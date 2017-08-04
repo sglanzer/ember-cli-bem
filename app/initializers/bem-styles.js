4e907edafdf129a400b45b988c77783bef35af24
Steven Glanzer <sglanzer@accedian.com>
2017-08-04 11:42:48 -0400

    Further attempts at :--this

---
+++ app/initializers/bem-styles.js
@@ -0,0 +1,35 @@
+import Ember from 'ember'
+import podNames from 'ember-cli-bem/pod-names'
+import BEM from 'ember-cli-bem/mixins/bem'
+
+const {
+  Component,
+  ComponentLookup,
+  computed,
+  getOwner
+} = Ember
+
+// ComponentLookup.reopen({
+//   componentFor(name, owner) {
+//     owner = owner.hasRegistration ? owner : getOwner(this)
+
+//     debugger
+
+//     if (podNames[name] && !owner.hasRegistration(`component:${name}`)) {
+//       owner.register(`component:${name}`, Component)
+//     }
+
+//     return this._super(...arguments)
+//   }
+// })
+
+Component.reopen(BEM, {
+
+})
+
+export function initialize () {}
+
+export default {
+  name: 'bem-styles',
+  initialize
+}
